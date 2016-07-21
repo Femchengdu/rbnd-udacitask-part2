cf106aedb2a2b1b9e6ce157fa638d19beb8c1c8f
Femi Ajayi <femiajayi705@gmail.com>
2016-07-22 02:44:58 +0800

    Added errors for invalid priority, out of range delete and invalid format

--- lib/todo.rb
+++ lib/todo.rb
@@ -5,7 +5,16 @@ class TodoItem
   def initialize(description, options={})
     @description = description
     @due = options[:due] ? Date.parse(options[:due]) : options[:due]
-    @priority = options[:priority]
+    check_priority options[:priority]
+  end
+
+  def check_priority option
+    priority_levels = ["high", "low", "medium", nil]
+    if priority_levels.include? option
+      @priority = option
+    else
+      raise UdaciListErrors::InvalidPriorityValue, "Invalid priority value!"
+    end
   end
 
   def details
